e331aaebb7fd7ef482564655fb18d1369cd5664f
pinje <98069681+pinje0@users.noreply.github.com>
2022-11-19 15:35:06 +0700

    Update main-program.py

--- main-program.py
+++ main-program.py
@@ -12,14 +12,25 @@ currentYear = datetime.now().year
 print(currentDay, "/", currentMonth, "/", currentYear)
 
 # Input Dictionaries
-# Key : Menu | Value : Harga
+# Key : Menu | Value : Harga [Key-Value Pair]
+
+# Dict menu
 menu = {
-    "Ayam Goreng": 15000,
-    "Burger King": 25000,
-    "Kentang Goreng": 10000,
-    "Es Teh Manis": 5000,
-    "Coca-cola": 12000,
+    "ayam goreng": 15000,
+    "burger king": 25000,
+    "kentang goreng": 10000,
+    "nasi goreng": 15000,
 }
+
+# Dict minuman
+minuman = {
+    "es teh manis": 5000,
+    "coca cola": 7000,
+    "jus jeruk": 10000,
+    "jus alpukat": 15000,
+}
+
+
 
 # Ngebungkus/wrap semua function menjadi 1, bernama main function() untuk dijadiin neested function
 def main():
@@ -31,44 +42,60 @@ def main():
         print("===================================================")
         print("Pembelian diatas Rp100.000,- mendapatkan diskon 15%")
         print("===================================================")
+        
         # Untuk input, harus ketik sesuai dictionaries menu{}, karna python case-sensitive language
-        beli = input("Pilih Menu : ")
+        # Input Makanan
+        beli1 = input("Pilih Menu : ")
+        jumlah1 = int(input("Jumlah Pesanan : "))
+        print("\n")
+
+        # Input Minuman
+        print("=================== Daftar Minum ==================")
+        for i in minuman:
+            print(i,"\t  | Rp.",minuman[i],)
+        # Input Minuman
+        beli2 = input("Pilih Minum : ")
+        jumlah2 = int(input("Jumlah Pesanan: "))
+        print("\n")
 
         # PERHITUNGAN UNTUK PEMBAYARAN
-        # Jumlah input cuma bisa 1, untuk nambah input harus memakai null/empty list(array [in Python])
-        jumlah = int(input("Jumlah Pesanan : "))
-        bayar = jumlah * menu[beli]
+        bayar = (menu[beli1] * jumlah1) + (minuman[beli2] * jumlah2)
         if bayar > 100000:
             diskon = bayar * 15 / 100
             total = bayar - diskon
         else:
             total = bayar
-            bayar = jumlah * menu[beli]
+            bayar = (menu[beli1] * jumlah1) + (minuman[beli2] * jumlah2)
 
         # Print Detail Pembayaran
         print("============== Detail Pembayaran ==================")
-        print("Menu yang di pesan       : ", beli)
-        print("Jumlah yang dipesan      : ", jumlah)
+        print("Menu yang di pesan       : ", beli1, "dan", beli2)
+        print(
+            f"Jumlah yang dipesan      :  {jumlah1} Makanan dan {jumlah2} Minuman",
+        )
         print("Total Biaya              : ", bayar)
         print("Total yang harus dibayar : ", total)
-        bayar = int(input("Bayar         :  "))
-        kembalian = bayar - total
+        pembayaran = int(input("Bayar         :  "))
+        kembalian = pembayaran - total
 
         # If-else statement jika uang yang di bayar perlu 'kembalian' , 'pas' atau 'kurang'
         # UANG PAS
-        if bayar == total:
-            print("Uang anda pas, Terima kasih telah berbelanja")
+        if pembayaran == total:
+            print("Uang anda pas\n")
+            tambah()
 
         # KEMBALIAN
-        elif bayar > total:
+        elif pembayaran > total:
             print("Kembalian        : ", kembalian)
-            print("===== Terima Kasih =====")
+            tambah()
 
         # UANG TIDAK CUKUP
-        elif bayar < total:
+        elif pembayaran < total:
 
             # MENANYAKAN APAKAH INGIN MEMESAN PESAN ULANG
-            tanya = input("Maaf uang anda tidak cukup, Apakah anda ingin memesan ulang? [y/t]:")
+            tanya = input(
+                "Maaf uang anda tidak cukup, Apakah anda ingin memesan ulang? [y/t]:"
+                )
             if tanya == "y":
                 pemilihan()
             elif tanya == "t":
@@ -76,7 +103,9 @@ def main():
             else:
                 # MENANYAKAN UNTUK KEDUA KALI JIKA INGIN MEMESAN ULANG
                 print("Pilihan yang anda masukan salah!")
-                tanya = tanya = input("Maaf uang anda tidak cukup, Apakah anda ingin memesan ulang? [y/t]:")
+                tanya = tanya = input(
+                    "Maaf uang anda tidak cukup, Apakah anda ingin memesan ulang? [y/t]:"
+                )
                 if tanya == "y":
                     pemilihan()
                 elif tanya == "t":
@@ -84,6 +113,18 @@ def main():
                 else:
                     # JIKA PILIHAN SELAIN Y/T DIKELUARKAN LAGI, MAKA PROGRAM AKAN MEMAKSA LOGOUT
                     print("Pilihan yang anda masukan salah!, Anda Terpaksa Logout!")
+    
+    # function tambah(), Jika ingin memesan makanan lagi
+    def tambah():
+        print("=====================================")
+        tanya = input("Apakah anda ingin memesan lagi? [y/t] : ")
+        print("=====================================\n")
+        if tanya == "y":
+            pemilihan()
+        elif tanya == "t":
+            print("===== Terima Kasih ======")
+        else:
+            print("Pilihan yang anda masukan salah!")
 
     # Deklarasi function untuk login
     def get_login():
