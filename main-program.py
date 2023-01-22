f3bec05f8863154a3f7f463e1747424c7c25b3c5
melvin austin <98069681+pinje0@users.noreply.github.com>
2023-01-22 21:19:00 +0700

    Update main-program.py

--- main-program.py
+++ main-program.py
@@ -2,6 +2,15 @@ print("|=================================================|")
 print("|==================== Restoran ===================|")
 print("|=================================================|\n")
 
+# Import getpass
+import getpass
+
+# Import pwinput
+import pwinput
+
+# Install module 'pwinput' in shell
+# pip install pwinput
+
 # Import date time
 from datetime import datetime
 
@@ -12,27 +21,27 @@ currentYear = datetime.now().year
 print(currentDay, "/", currentMonth, "/", currentYear)
 
 
-# Input Dictionaries
-# Key : Menu | Value : Harga [Key-Value Pair]
-# Dict menu
-menu = {
-    "ayam goreng": 15000,
-    "burger king": 25000,
-    "kentang goreng": 10000,
-    "nasi goreng": 15000,
-}
-
-# Dict minuman
-minuman = {
-    "es teh manis": 5000,
-    "coca cola": 7000,
-    "jus jeruk": 10000,
-    "jus alpukat": 15000,
-}
-
-
 # Ngebungkus/wrap semua function menjadi 1, bernama main function() untuk dijadiin neested function
 def main():
+    # Input Dictionaries
+    # Key : Menu | Value : Harga [Key-Value Pair]
+    
+    # Dict menu
+    menu = {
+        "ayam goreng": 15000,
+        "burger king": 25000,
+        "kentang goreng": 10000,
+        "nasi goreng": 15000,
+    }
+    
+    # Dict minuman
+    minuman = {
+        "es teh manis": 5000,
+        "coca cola": 7000,
+        "jus jeruk": 10000,
+        "jus alpukat": 15000,
+    }
+    
     # Deklarasi function pemilihan
     def pemilihan():
         print("=================== Daftar Menu ===================")
@@ -42,13 +51,12 @@ def main():
         print("Pembelian diatas Rp100.000,- mendapatkan diskon 15%")
         print("===================================================")
         
-        # Untuk input, harus ketik sesuai dictionaries menu{}, karna python case-sensitive language
-        # Input Makanan
+    # Untuk input, harus ketik sesuai dictionaries menu{}, karna python case-sensitive language
+    # Input Makanan
         beli1 = input("Pilih Menu : ")
         jumlah1 = int(input("Jumlah Pesanan : "))
         print("\n")
-
-        # Input Minuman
+    # Input Minuman
         print("=================== Daftar Minum ==================")
         for i in minuman:
             print(i,"\t  | Rp.",minuman[i],)
@@ -56,7 +64,6 @@ def main():
         beli2 = input("Pilih Minum : ")
         jumlah2 = int(input("Jumlah Pesanan: "))
         print("\n")
-
         # PERHITUNGAN UNTUK PEMBAYARAN
         bayar = (menu[beli1] * jumlah1) + (minuman[beli2] * jumlah2)
         if bayar > 100000:
@@ -65,7 +72,6 @@ def main():
         else:
             total = bayar
             bayar = (menu[beli1] * jumlah1) + (minuman[beli2] * jumlah2)
-
         # Print Detail Pembayaran
         print("============== Detail Pembayaran ==================")
         print("Menu yang di pesan       : ", beli1, "dan", beli2)
@@ -76,21 +82,17 @@ def main():
         print("Total yang harus dibayar : ", total)
         pembayaran = int(input("Bayar         :  "))
         kembalian = pembayaran - total
-
-        # If-else statement jika uang yang di bayar perlu 'kembalian' , 'pas' atau 'kurang'
-        # UANG PAS
+    # If-else statement jika uang yang di bayar perlu 'kembalian' , 'pas' atau 'kurang'
+    # UANG PAS
         if pembayaran == total:
             print("Uang anda pas\n")
-            tambah()
-
+            pesan_ulang()
         # KEMBALIAN
         elif pembayaran > total:
             print("Kembalian        : ", kembalian)
-            tambah()
-
+            pesan_ulang()
         # UANG TIDAK CUKUP
         elif pembayaran < total:
-
             # MENANYAKAN APAKAH INGIN MEMESAN PESAN ULANG
             tanya = input(
                 "Maaf uang anda tidak cukup, Apakah anda ingin memesan ulang? [y/t]:"
@@ -113,8 +115,8 @@ def main():
                     # JIKA PILIHAN SELAIN Y/T DIKELUARKAN LAGI, MAKA PROGRAM AKAN MEMAKSA LOGOUT
                     print("Pilihan yang anda masukan salah!, Anda Terpaksa Logout!")
     
-    # function tambah(), Jika ingin memesan makanan lagi
-    def tambah():
+    # function pesan_ulang(), Jika ingin memesan makanan lagi
+    def pesan_ulang():
         print("=====================================")
         tanya = input("Apakah anda ingin memesan lagi? [y/t] : ")
         print("=====================================\n")
@@ -132,7 +134,7 @@ def main():
 
         # input username dan password
         username = input("Masukan Username Anda: ")
-        password = input("Masukan Password: ")
+        password = pwinput.pwinput(prompt='Masukkan Password: ', mask='*')
 
         # Menentukan username dan password
         if username == "melvin" and password == "melvin":
@@ -142,7 +144,7 @@ def main():
             print("Login Gagal. Coba Lagi..")
             get_login()
 
-    # Membuat program nge-execute function get_login() pertama kali / entry point of the program
+    # Membuat program nge-execute function get_login() pertama kali
     if __name__ == "__main__":
         get_login()
 
